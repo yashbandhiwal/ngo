175f47350f71b4450a09ae08c6b87e1184c6399d
yashbandhiwal <yash.bandhiwal1995@gmail.com>
2023-12-15 05:30:01 +0530

    medicine & illness & checkup

---
+++ ngo_mongo/Illness.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const IllnessSchema = new mongoose.Schema({
+
+    petCode:{
+        type:String,
+    },
+    petName:{
+        type:String,
+    },
+    illness:{
+        type:String,
+    },
+    medicineId:[{
+        type: mongoose.Schema.ObjectId,
+        ref: 'Medicine',
+        required: true
+    }],
+
+
+})
---
+++ ngo_mongo/checkup.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const IllnessSchema = new mongoose.Schema({
+
+    petCode:{
+        type:String,
+    },
+    petName:{
+        type:String,
+    },
+    health:{
+        type:String,
+    },
+    illness:{
+        type:String,
+    },
+    medicineId:[{
+        type: mongoose.Schema.ObjectId,
+        ref: 'Medicine',
+        required: true
+    }],
+
+
+})
